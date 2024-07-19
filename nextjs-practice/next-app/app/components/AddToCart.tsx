3d1c334b59e57d70687d67f4ae34414c4111ca95
Kenny Doan <kdoan93@gmail.com>
2024-07-19 17:48:03 -0400

    Part 1 Next.js practice

---
+++ nextjs-practice/next-app/app/components/AddToCart.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const AddToCart = () => {
+  return (
+    <button className='btn btn-primary' onClick={() => console.log('Clicked')}>
+        Add to Cart
+    </button>
+  )
+}
+
+export default AddToCart
